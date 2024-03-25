fdf07e5ac93bc8a869119f9965d7b4de3a5ec598
Melrain <yezyd92@icoud.com>
2024-03-25 20:27:22 +0800

    done

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const ws = require('ws');
 const server = new ws.Server({ port: 8080 });
 
-console.log('Server started at ws://localhost:8080');
-
 server.on('connection', (socket) => {
-  console.log('New connection');
+  console.log('New userConnected');
+
   socket.on('message', (message) => {
-    console.log(message);
-    socket.send(`${message}`);
+    const b = Buffer.from(message);
+    console.log(`Received: ${b.toString()}`);
+    socket.send(`Echo: ${message}`);
   });
 });
